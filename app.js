7df4d61a8ea15193391f2566cc8934e9af44a561
Giraud Julemis <giraud.julemis@gmail.com>
2022-11-11 13:52:56 -0500

    build express server

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//routes
+app.get('/hello', (req, res) => {
+  res.send('Task manager App');
+})
+
+app.listen(port, console.log(`Server is listening on port ${port}...`));
