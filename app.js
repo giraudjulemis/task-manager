29738a0681a3f48266c724476f56e0eca0656404
Giraud Julemis <giraud.julemis@gmail.com>
2022-11-12 15:00:23 -0500

    build update and delete routes

--- app.js
+++ app.js
@@ -6,12 +6,9 @@ const connectDB = require('./db/connect');
 const dotenv = require('dotenv').config();
 
 //middleware
+app.use(express.static('./public'));
 app.use(express.json());
 //routes
-app.get('/hello', (req, res) => {
-  res.send('Task manager App');
-});
-
 app.use('/api/v1/tasks', tasks);
 
 const start = async () => {
